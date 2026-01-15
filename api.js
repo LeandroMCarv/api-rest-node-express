bf96babcdfd8ac4c65834c2f0c3ae89f6c7ebb1e
LeandroMCarv <>
2026-01-15 14:56:43 -0300

    init: initial commit

---
+++ api.js
@@ -0,0 +1,6 @@
+/*API = Application Programming Interface
+Uma API REST significa Representational State Transfer
+Usa os principais métodos HTTP: GET, POST, PUT/PATCH e DELETE
+*/
+
+const express = require('express');
