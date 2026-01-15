94288c9dd96f10236e5073da54b7aac24219437e
LeandroMCarv <>
2026-01-15 15:23:41 -0300

    feat: responses to get methods

--- api.js
+++ api.js
@@ -1,6 +1,29 @@
 /*API = Application Programming Interface
 Uma API REST significa Representational State Transfer
 Usa os principais métodos HTTP: GET, POST, PUT/PATCH e DELETE
+JSON = JavaScript Object Notation
+Express = Framework do Node.js para construção de APIs
 */
 
-const express = require('express');
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+const arrResponse = [
+  { name: "Leandro", company: "Contabilize" },
+  { name: "Dudu", company: "MW Informatica" },
+];
+
+//tratamento do get no endpoint raiz
+app.get("/", (req, res) => {
+  res.send("API em funcionamento!");
+});
+
+//tratamento do get no get no endpoint /json
+app.get("/json", (req, res) => {
+  res.json(arrResponse);
+});
+
+app.listen(PORT, () => {
+  console.log("O servidor está rodando...");
+});
